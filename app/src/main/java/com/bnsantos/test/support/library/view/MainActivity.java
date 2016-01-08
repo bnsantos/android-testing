73089a2eb1da49d8db5c3268257c043d2ab248f1
Bruno Santos <brunonascimentosantos@gmail.com>
2016-01-08 15:25:35 -0200

    left header

--- app/src/main/java/com/bnsantos/test/support/library/view/MainActivity.java
+++ app/src/main/java/com/bnsantos/test/support/library/view/MainActivity.java
@@ -29,8 +29,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         updateTitle();
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
